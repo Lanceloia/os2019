7031e772b2f5a1b195e84aae0a77db5fa5d8d166
tracer-nju <tracer@nju.edu.cn>
2019-04-04 11:09:28 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 9fd6aca0fc884c75b0938aa81dcc46d4   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  11:09:28 up  1:03,  1 user,  load average: 2.30, 1.38, 1.01

--- os-workbench/kernel/src/alloc.c
+++ os-workbench/kernel/src/alloc.c
@@ -108,9 +108,9 @@ static void free_init(uintptr_t begin, uintptr_t end){
 static void free_print(){
   mem_block *block = free.head->next;
   printf("free_print():\n");
-  printf("  id   state   size    begin      next\n");
+  printf("  id   state       size      begin      next\n");
   while(block != free.tail){
-    printf("%4d   %d  %10dKB  %10d  %4d\n",
+    printf("%4d     %d  %10dKB  %10d  %4d\n",
         block->id, block->state, block->size / 1024,
         block->begin, block->next->id);
     block = block->next;
