2de872cc7a1cbb9d6e94a84a153853810c34ba80
tracer-nju <tracer@nju.edu.cn>
2019-05-18 01:45:37 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 76f862bb15ed43f3aade98c8be992daa   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  01:45:37 up  1:04,  1 user,  load average: 1.64, 0.68, 0.47

--- os-workbench/kernel/src/kthreads.c
+++ os-workbench/kernel/src/kthreads.c
@@ -22,7 +22,7 @@ static _Context *kmt_context_switch(_Event ev, _Context *ctx) {
       current = tasks_list_head;
     else
       current = current->next;
-    printf("%d ", _cpu());
+    // printf("%d ", _cpu());
   } while (!(current->state == STARTED || current->state == RUNNABLE));
  
   current->state = RUNNING;
