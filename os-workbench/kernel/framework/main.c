8e740fae7e97e1df113e4340de70ba606845d2cf
tracer-nju <tracer@nju.edu.cn>
2019-05-18 02:00:02 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 76f862bb15ed43f3aade98c8be992daa   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  02:00:02 up  1:19,  1 user,  load average: 1.63, 1.16, 0.85

--- os-workbench/kernel/framework/main.c
+++ os-workbench/kernel/framework/main.c
@@ -35,8 +35,8 @@ static void create_threads() {
   kmt->create(pmm->alloc(sizeof(task_t)), "test-thread-2: producer", producer, NULL);
   kmt->create(pmm->alloc(sizeof(task_t)), "test-thread-3: consumer", consumer, NULL);
   kmt->create(pmm->alloc(sizeof(task_t)), "test-thread-4: consumer", consumer, NULL);
-  kmt->sem_init(&empty, "buffer-empty", maxk / 2);
-  kmt->sem_init(&full, "buffer-full", maxk - maxk / 2);
+  kmt->sem_init(&empty, "buffer-empty", maxk);
+  kmt->sem_init(&full, "buffer-full", 0);
   kmt->sem_init(&mutex, "mutex", 1);
 }
 
