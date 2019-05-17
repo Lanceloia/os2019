5108a466588ea0eb7ea96b942108d21adae31fab
tracer-nju <tracer@nju.edu.cn>
2019-05-18 01:50:03 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 76f862bb15ed43f3aade98c8be992daa   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  01:50:03 up  1:09,  1 user,  load average: 1.39, 1.10, 0.71

--- os-workbench/kernel/include/kthreads.h
+++ os-workbench/kernel/include/kthreads.h
@@ -45,7 +45,7 @@ task_t wait[MAX_CPU] = {};
 void task_wait(void *arg) {
   while (1) {
     for(volatile int i = 0; i < 1000; i++);
-    printf("nmsl ");
+    printf("\nnmsl %d", _cpu());
     for(volatile int i = 0; i < 1000; i++);
     _yield();
   }
