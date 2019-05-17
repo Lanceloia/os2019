697119da99c8fd63309765131c5678bc06e64d6b
tracer-nju <tracer@nju.edu.cn>
2019-05-17 19:12:26 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 6dea656cdd0646e9991fc87401364e90   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  19:12:26 up  5:08,  1 user,  load average: 1.03, 0.92, 0.96

--- os-workbench/kernel/src/kthreads.c
+++ os-workbench/kernel/src/kthreads.c
@@ -54,7 +54,7 @@ void kmt_init() {
   os->on_irq(INT32_MAX, _EVENT_NULL, kmt_context_switch);
   kmt_spin_init(&tasks_list_mutex, "tasks-list-mutex");
   kmt_spin_init(&current_tasks_mutex, "current-tasks-mutex");
-  kmt_create(&task_null, "task-null", null, NULL);
+  //kmt_create(&task_null, "task-null", null, NULL);
 }
 
 /* tasks
