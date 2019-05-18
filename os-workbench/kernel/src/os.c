e5fa7d3a7c5db1671a2c5aa1f2b2ea836032b022
tracer-nju <tracer@nju.edu.cn>
2019-05-19 02:00:25 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 3fe665e9cf364263b91c184e4e9281fa   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  02:00:25 up  8:01,  1 user,  load average: 1.61, 1.31, 1.24

--- os-workbench/kernel/src/os.c
+++ os-workbench/kernel/src/os.c
@@ -1,18 +1,7 @@
 #include <common.h>
 #include <os.h>
 
-device_t *dev_lookup(const char *name);
-
-void echo_task(void *name) {
-  device_t *tty = dev_lookup(name);
-  while (1) {
-    char line[128], text[128];
-    sprintf(text, "(%s) $ ", name); //tty->ops->write(tty, 0, text, sizeof(text));
-    int nread = tty->ops->read(tty, 0, line, sizeof(line));
-    line[nread - 1] = '\0';
-    sprintf(text, "Echo: %s.\n", line); //tty->ops.write(tty, 0, text, sizeof(text));
-  }
-}
+extern void echo_task(void *name);
 
 static void os_init() {
   pmm->init();
