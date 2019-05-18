e34ea50e3f0724fca0c60b57600b3f2c4602a17b
tracer-nju <tracer@nju.edu.cn>
2019-05-18 23:14:57 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 3fe665e9cf364263b91c184e4e9281fa   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  23:14:57 up  5:15,  1 user,  load average: 2.30, 2.22, 2.04

--- os-workbench/kernel/src/kthreads.c
+++ os-workbench/kernel/src/kthreads.c
@@ -109,7 +109,7 @@ static void kmt_spin_lock(spinlock_t *lk) {
 
   pushcli();
   while(_atomic_xchg(&lk->locked, LOCKED))
-    SLEEP(256);
+    SLEEP(1024);
   lk->cpu = _cpu();
   __sync_synchronize();
 }
