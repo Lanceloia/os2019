b40c441b9fa344330a0e4148fdc26436a6c082e8
tracer-nju <tracer@nju.edu.cn>
2019-05-19 02:05:43 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 3fe665e9cf364263b91c184e4e9281fa   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  02:05:43 up  8:06,  1 user,  load average: 2.86, 1.98, 1.53

--- os-workbench/kernel/src/devices/tty/tty.c
+++ os-workbench/kernel/src/devices/tty/tty.c
@@ -272,10 +272,10 @@ void tty_task(void *arg) {
 void echo_task(void *name) {
   device_t *tty = dev_lookup(name);
   while (1) {
-    char line[128]={}, text[128]={};
-    sprintf(text, "(%s) $ ", name); tty_write(tty, 0, text, sizeof(text));
+    char line[128], text[128];
+    sprintf(text, "(%s) $ ", name); tty_write(tty, 0, text, strlen(text));
     int nread = tty->ops->read(tty, 0, line, sizeof(line));
     line[nread - 1] = '\0';
-    sprintf(text, "Echo: %s.\n", line); tty_write(tty, 0, text, sizeof(text));
+    sprintf(text, "Echo: %s.\n", line); tty_write(tty, 0, text, strlen(text));
   }
 }
