074bc375991b14a6d2b63976d6d7b146b7dfff06
tracer-nju <tracer@nju.edu.cn>
2019-05-19 00:06:40 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 3fe665e9cf364263b91c184e4e9281fa   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  00:06:40 up  6:07,  1 user,  load average: 2.46, 2.63, 2.26

--- os-workbench/kernel/framework/main.c
+++ os-workbench/kernel/framework/main.c
@@ -1,41 +1,5 @@
 #include <kernel.h>
 #include <klib.h>
-#include "../include/common.h"
-
-sem_t empty, full, mutex;
-const int maxk = 4;
-int cnt = 0;
-
-static void producer(void *arg) {
-  while(1) {
-    kmt->sem_wait(&empty);
-    kmt->sem_wait(&mutex);
-    cnt ++;
-    printf("%d%c ", cnt, _cpu()+'a');
-    kmt->sem_signal(&mutex);
-    kmt->sem_signal(&full);
-  }
-}
-
-static void consumer(void *arg) {
-  while(1) {
-    kmt->sem_wait(&full);
-    kmt->sem_wait(&mutex);
-    cnt --;
-    kmt->sem_signal(&mutex);
-    kmt->sem_signal(&empty);
-  }
-}
-
-static void create_threads() {
-  kmt->create(pmm->alloc(sizeof(task_t)), "test-thread-1: producer", producer, NULL);
-  kmt->create(pmm->alloc(sizeof(task_t)), "test-thread-2: producer", producer, NULL);
-  kmt->create(pmm->alloc(sizeof(task_t)), "test-thread-3: consumer", consumer, NULL);
-  kmt->create(pmm->alloc(sizeof(task_t)), "test-thread-4: consumer", consumer, NULL);
-  kmt->sem_init(&empty, "buffer-empty", maxk);
-  kmt->sem_init(&full, "buffer-full", 0);
-  kmt->sem_init(&mutex, "mutex", 1);
-}
 
 int main() {
   _ioe_init();
@@ -43,7 +7,7 @@ int main() {
 
   // call sequential init code
   os->init();
-  create_threads();
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
+
