dd51dc65f5a8cbf97d0133246f68e119e3a4e9b6
tracer-nju <tracer@nju.edu.cn>
2019-05-19 05:18:59 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 3fe665e9cf364263b91c184e4e9281fa   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  05:19:00 up 11:19,  1 user,  load average: 2.17, 2.24, 2.10

--- os-workbench/kernel/framework/main.c
+++ os-workbench/kernel/framework/main.c
@@ -35,23 +35,23 @@ static void consumer(void *arg) {
 static void mogician(void *arg) {
   task_t *producer_task = NULL, *consumer_task = NULL;
   while (1) {
-    if (!producer_task && cnt <  maxk / 2 && rand() % 100 == 0) {
+    if (!producer_task && cnt <  maxk / 2 && rand() % 10 == 0) {
       producer_task = pmm->alloc(sizeof(task_t));
       kmt->create(producer_task, "producer", producer, NULL);
     }
     
-    if (!consumer_task && cnt >= maxk / 2 && rand() % 100 == 0) {
+    if (!consumer_task && cnt >= maxk / 2 && rand() % 10 == 0) {
       consumer_task = pmm->alloc(sizeof(task_t));
       kmt->create(consumer_task, "consumer", consumer, NULL);
     }
 
-    if (producer_task && rand() % 50 == 0) {
+    if (producer_task && rand() % 20 == 0) {
       kmt->teardown(producer_task);
       pmm->free(producer_task);
       producer_task = NULL;
     }
 
-    if (consumer_task && rand() % 50 == 0) {
+    if (consumer_task && rand() % 20 == 0) {
       kmt->teardown(consumer_task);
       pmm->free(consumer_task);
       consumer_task = NULL;
