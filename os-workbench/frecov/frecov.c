c1e0a990258453756e80a140f07de73d156b3bed
tracer-nju <tracer@nju.edu.cn>
2019-06-04 01:27:59 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 16bb3cb7da6f4da09e1686be3dda7b69   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  01:27:59 up  8:50,  1 user,  load average: 1.09, 1.19, 1.22

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -204,7 +204,7 @@ void output_bmp(char *data, struct YELLO_BMP *yb){
     color[2] = (yb->color >> 16) & 0xff;
     */ 
   for(int i = 0; i < yb->height; i ++)
-    for(int j = 0; j <  ((yb->width * 24 + 31) / 8); j ++)
+    for(int j = 0; j <  ((yb->width + 3) / 4); j ++)
       fwrite(color + (j % 3), sizeof(char), 1, fp);
   fclose(fp);
 }
