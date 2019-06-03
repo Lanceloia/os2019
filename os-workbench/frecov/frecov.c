238cdfc96b799d57bb9915c873b1b5563a3ef56b
tracer-nju <tracer@nju.edu.cn>
2019-06-03 17:10:17 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 16bb3cb7da6f4da09e1686be3dda7b69   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  17:10:17 up 32 min,  1 user,  load average: 1.45, 1.89, 1.45

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -24,7 +24,7 @@ struct FAT32 {
   int DBR_sec_index;// the index of DBR sector,              0x32, 2
   char volume_label[12]; // the label of volime,             0x47, 11
   char fat_type[9]; // the type of FAT system,               0x52, 8
-} fat32; 
+} fat32;
 
 void print_FAT32_info(struct FAT32 *f) {
   debug("sector_size", f->sector_size);
@@ -45,8 +45,6 @@ int read_num(void *p, int len) {
   return ret;
 }
 
-
-
 void read_fat32_info(struct FAT32 *f, void *data) {
   f->sector_size = read_num(data + 0x0b, 2);
   f->cluster_size = read_num(data + 0x0d, 1);
@@ -60,6 +58,39 @@ void read_fat32_info(struct FAT32 *f, void *data) {
 }
 
 
+struct YELLO_BMP {
+  int color, clusters_size;
+  char filename[256];
+  int clusters[1024];
+} yello_bmp[128];
+
+int tot_bmp = 0;
+
+void push_cluster(struct YELLO_BMP *yb, int offset) {
+  yb->clusters[yb->clusters_size ++] = offset;
+}
+
+void init_yello_bmp(void *data, int offset){
+  yello_bmp[tot_bmp].color = read_num(data + offset + 0x54, 3);
+  push_cluster(&yello_bmp[tot_bmp], offset);
+  tot_bmp ++;
+}
+
+int search_yello_bmp(void *data, int offset){
+  if(((char *)data)[offset] == 'B' && ((char *)data)[offset + 1] == 'M')
+    init_yello_bmp(data, offset);
+}
+
+void show_yello_bmp(){
+  for(int i = 0; i < 128; i ++) {
+    if(yello_bmp[i].clusters_size == 0)
+      break;
+    debug("yello_bmp + ", i);
+    debug("color", yello_bmp[i].color);
+    debug("clusters_size", yello_bmp[i].clusters_size);
+  }
+}
+
 
 int main(int argc, char *argv[]) {
   int fd = open(argv[1], O_RDWR);
@@ -69,6 +100,10 @@ int main(int argc, char *argv[]) {
   close(fd);
 
   read_fat32_info(&fat32, imgmap);
+  for(int i = 0; i < 64 MB; i += 0x200) {
+    search_yello_bmp(imgmap, i);
+  }
+  show_yello_bmp();
 
   return 0;
 }
