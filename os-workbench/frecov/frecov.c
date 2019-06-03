c57c8f3e7a2baacf696539297ef64623b8797869
tracer-nju <tracer@nju.edu.cn>
2019-06-03 23:20:11 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 16bb3cb7da6f4da09e1686be3dda7b69   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  23:20:11 up  6:42,  1 user,  load average: 0.82, 1.22, 1.20

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -191,7 +191,7 @@ int judge_attribution(void *data, int offset) {
 }
 
 void output_bmp(char *data, struct YELLO_BMP *yb){
-  FILE *fp = fopen(yb->filename, "wb+");
+  FILE *fp = fopen(yb->filename, "w+");
   if(!fp) return;
   for(int i = 0; i < yb->clusters_size; i ++) {
     fwrite(data + yb->clusters[i], sizeof(char), fat32.sector_size, fp);
