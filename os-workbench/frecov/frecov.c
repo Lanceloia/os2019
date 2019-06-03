4e2fb6fb7cc93aaf0be2d99656c9873a294e99eb
tracer-nju <tracer@nju.edu.cn>
2019-06-03 23:22:54 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 16bb3cb7da6f4da09e1686be3dda7b69   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  23:22:54 up  6:45,  1 user,  load average: 1.79, 1.34, 1.24

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -183,6 +183,8 @@ int judge_attribution(void *data, int offset) {
     read_num(data + offset + 0x6, 3) &&
     read_num(data + offset + 0x6, 3) ==
     read_num(data + offset + 0x9, 3) &&
+    read_num(data + offset + 0x9, 3) ==
+    read_num(data + offset + 0xc, 3) &&
     read_num(data + offset, 3) != 0x0
   )
     return 1;//{printf("color = %x\n", read_num(data + offset, 3)); return 1; }
