3f02e954ece6fa26625a6c5df392464fa55ce321
tracer-nju <tracer@nju.edu.cn>
2019-06-04 02:26:22 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 16bb3cb7da6f4da09e1686be3dda7b69   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  02:26:22 up  9:48,  1 user,  load average: 1.20, 1.07, 1.06

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -63,27 +63,18 @@ void read_fat32_info(char *data) {
   print_FAT32_info();
 } 
 
-struct YELLO_BMP {
-  int offset;
-  int clusters_size;
+struct BMP_INFO {
+  int offset, size;
   int color, width, height;
   char filename[256];
-  int clusters[1024];
-} yello_bmp[128];
+} BMP_INFO[128];
 
 int tot_bmp = 0;
 
-void push_cluster(struct YELLO_BMP *yb, int offset) {
-  yb->clusters[yb->clusters_size ++] = offset;
-}
-
-int read_unicode(char dest[], char src[], int len) {
+void read_unicode(char dest[], char src[], int len) {
   for(int i = 0; i < len; i ++) {
     dest[i] = src[2 * i];
-    if(dest[i] == '\0')
-      return 0;
   }
-  return 1;
 }
 
 int is_valid(char ch) {
@@ -96,98 +87,87 @@ int is_valid(char ch) {
 
 struct FILE {
   char filename[256];
-  int offset;
+  int position;
 } file[256];
 int tot_file;
 
-void read_name_offset(char *data, int offset) {
+char buf[64][256] = {};
+
+int read_name_position_do(char *data, int offset, char *name, int *position) {
+  int top = 0, ret = 0; name[0] = '\0';
+  while(*(data + offset + 0x0b) == (char)0x0f) {
+    read_unicode(buf[top], data + offset + 0x01, 5);
+    read_unicode(buf[top] + 5, data + offset + 0x0e, 6);
+    read_unicode(buf[top] + 11, data + offset + 0x1c, 2);
+    top ++; offset += 0x20; ret += 0x20;
+  }
+
+  while(top --) {
+    strcat(name, buf[top]);
+  }
+
+  *position = read_num(data + offset + 0x34, 2) << 16;
+  *position += read_num(data + offset + 0x3a, 2);
+
+  return ret;
+}
+
+int read_name_position(char *data, int offset) {
   if(*(data + offset + 0x0b) == (char)0x0f) {
-    char buf[256] = {0x01, 0x01, 0x01, 0x01, 0x01, 0x01};
-    if(read_unicode(buf, data + offset + 0x01, 5))
-      if(read_unicode(buf + 5, data + offset + 0x0e, 6))
-        read_unicode(buf + 11, data + offset + 0x1c, 2);
-
-    if(is_valid(buf[0]) && is_valid(buf[1]) && is_valid(buf[2])) {
-      strcpy(file[tot_file].filename, buf);
-      file[tot_file].offset = read_num(data + offset + 0x34, 2) << 16;
-      file[tot_file ++].offset += read_num(data + offset + 0x3a, 2);
-    
-      if(strncmp(data + offset + 0x20, buf, 3) != 0) {
-        printf("%s\n", buf);
-      }
-    }
+    tot_file ++;
+    return read_name_position_do(data, offset,
+      file[tot_file - 1].filename,
+      &file[tot_file - 1].position);
   }
-  else {
-    if(*(data + offset + 0x06) == '~' && *(data + offset + 0x07) == '1') {
-      //printf("%s\n", data + offset);
-    }
+  return 0x20;
+}
+
+void search_bmp_name_position(char *data, int offset) {
+  int step = 0;
+  for(int i = 0; i < fat32.sector_size; i += step){
+    step = read_name_position(data, offset + i);
   }
 }
 
-int search_bmp_name(char *data, int offset) {
-  for(int i = 0; i < fat32.sector_size; i += 0x20){
-    read_name_offset(data, offset + i);
-  }
-  return 0;
-}
-
-void search_bmp_head(char *data, int offset) {
+/*
+void search_bmp_head_structure(char *data, int offset) {
   for(int j = 0; j < fat32.sector_size; j += 0x20) {
     if(data[offset + j] == 'B' && data[offset + j + 1] == 'M') {
-      // init_yello_bmp
-      yello_bmp[tot_bmp].color = read_num(data + offset + j + 0x54, 3);
-      yello_bmp[tot_bmp].width = read_num(data + offset + 0x12, 4);
-      yello_bmp[tot_bmp].height = read_num(data + offset + 0x16, 4);
-      yello_bmp[tot_bmp].offset = offset + j;
+      // init_BMP_INFO
+      BMP_INFO[tot_bmp].color = read_num(data + offset + j + 0x54, 3);
+      BMP_INFO[tot_bmp].width = read_num(data + offset + 0x12, 4);
+      BMP_INFO[tot_bmp].height = read_num(data + offset + 0x16, 4);
+      BMP_INFO[tot_bmp].offset = offset + j;
       for(int i = 0; i < tot_file; i ++)
-        if (file[i].offset == ((yello_bmp[tot_bmp].offset - 0x81c00) / 0x200))
-          strcpy(yello_bmp[tot_bmp].filename, file[i].filename);
-      push_cluster(&yello_bmp[tot_bmp], offset);
+        if (file[i].position == ((BMP_INFO[tot_bmp].offset - 0x81c00) / 0x200))
+          strcpy(BMP_INFO[tot_bmp].filename, file[i].filename);
       tot_bmp ++;
     }
   }
 }
+*/
 
 void show_file(){
   for(int i = 0; i < tot_file; i ++){
     printf("%d: %s, 0x%08x\n",
-    i, file[i].filename, file[i].offset);
+    i, file[i].filename, file[i].position);
   }
 }
 
-void show_yello_bmp(){
+/*
+void show_BMP_INFO(){
   for(int i = 0; i < tot_bmp; i ++) {
-    if(yello_bmp[i].clusters_size == 0)
-      break;
     printf("bmp_index: %d, ", i);
-    printf("color: 0x%x, ", yello_bmp[i].color);
-    printf("w: %d, h: %d, ", yello_bmp[i].width, yello_bmp[i].height);
-    printf("offset: 0x%x, ", yello_bmp[i].offset);
-    printf("filename: %s, ", yello_bmp[i].filename);
-    printf("clusters_size: %d\n", yello_bmp[i].clusters_size);
+    printf("color: 0x%x, ", BMP_INFO[i].color);
+    printf("w: %d, h: %d, ", BMP_INFO[i].width, BMP_INFO[i].height);
+    printf("offset: 0x%x, ", BMP_INFO[i].offset);
+    printf("filename: %s, ", BMP_INFO[i].filename);
   }
 }
+*/
 
-int judge_attribution(void *data, int offset) {
-  if(((char *)data)[offset] == 'B' && ((char *)data)[offset + 1] == 'M') {
-    return 0;
-  }
-  else if(read_num(data + offset, 3) == 
-    read_num(data + offset + 0x3, 3) &&
-    read_num(data + offset + 0x3, 3) ==
-    read_num(data + offset + 0x6, 3) &&
-    read_num(data + offset + 0x6, 3) ==
-    read_num(data + offset + 0x9, 3) &&
-    read_num(data + offset + 0x9, 3) ==
-    read_num(data + offset + 0xc, 3) &&
-    read_num(data + offset, 3) != 0x0
-  )
-    return 1;
-  else
-    return 2;
-}
-
-void output_bmp(char *data, struct YELLO_BMP *yb){
+/*
+void output_bmp(char *data, struct BMP_INFO *yb){
   FILE *fp = fopen(yb->filename, "wb+");
   if(!fp) return;
   printf("%x\n", *(int *)(data + yb->clusters[0]));
@@ -208,14 +188,7 @@ void output_bmp(char *data, struct YELLO_BMP *yb){
       fwrite(color + (j % 3), sizeof(char), 1, fp);
   fclose(fp);
 }
-
-void output2_bmp(char *data, struct YELLO_BMP *yb){
-  FILE *fp = fopen(yb->filename, "wb+");
-  if(!fp) return;
-  for(int i = 0; i < yb->clusters_size; i ++)
-    fwrite(data + yb->clusters[i], sizeof(char), fat32.sector_size, fp);
-  fclose(fp);
-}
+*/
 
 int main(int argc, char *argv[]) {
   int fd = open(argv[1], O_RDWR);
@@ -223,22 +196,19 @@ int main(int argc, char *argv[]) {
   imgmap = mmap(NULL, 64 MB, PROT_READ, MAP_SHARED, fd, 0);
   if (imgmap == (void *)-1) {debug2("mmap failed."); return 1;} 
   close(fd);
+
   read_fat32_info(imgmap);
   
   for(int i = 0; i < 32 MB; i += fat32.sector_size) {
-    if(judge_attribution(imgmap, i) == 2)
-      search_bmp_name(imgmap, i);
-  }
-
-  for(int i = 0; i < 32 MB; i += fat32.sector_size) {
-    if(judge_attribution(imgmap, i) == 0)
-      search_bmp_head(imgmap, i);
+    search_bmp_name_position(imgmap, i);
   }
 
   show_file();
-  show_yello_bmp();
+  show_BMP_INFO();
 
+  /*
   for(int i = 0; i < tot_bmp; i ++)
-    output2_bmp(imgmap, &yello_bmp[i]);
+    output2_bmp(imgmap, &BMP_INFO[i]);
+  */
   return 0;
 }
