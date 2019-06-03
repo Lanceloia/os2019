d0bfa8a839825f51594f3eab515ca9dbf5459e96
tracer-nju <tracer@nju.edu.cn>
2019-06-03 19:10:11 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 16bb3cb7da6f4da09e1686be3dda7b69   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  19:10:11 up  2:32,  1 user,  load average: 2.27, 2.18, 2.00

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -111,7 +111,10 @@ int search_yello_bmp(void *data, int offset){
     return 1;
   }
   else if(read_num(data + offset, 3) == 
-  read_num(data + offset + 0x3, 3))
+  read_num(data + offset + 0x3, 3) &&
+  read_num(data + offset + 0x6, 3) ==
+  read_num(data + offset + 0x9, 3)
+  )
     return 1;
   else
     return 0;
