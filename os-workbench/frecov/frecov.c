6490c06a26fa0e14e7a454c0240b148e93576e92
tracer-nju <tracer@nju.edu.cn>
2019-06-04 11:29:42 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: b4891eb03e6748ba870c61fd4ebd74e7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  11:29:42 up  1:21,  1 user,  load average: 1.64, 1.03, 0.83

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -211,7 +211,7 @@ int deep_search_bmp_name_position(char *data, int offset) {
   if(file[tot_file - 1].visited == 0) {
     file[tot_file - 1].visited = 1;
     cnt += search_bmp_name_position(
-      data, offset + (file[tot_file - 1].next_sector - 2)  * fat32.sector_size);
+      data, offset + (file[tot_file - 1].next_sector - 1)  * fat32.sector_size);
   }
   //}
   return cnt;
