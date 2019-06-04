d6bf525b661954a8a2bc166b92d79f3095d47a84
tracer-nju <tracer@nju.edu.cn>
2019-06-04 14:51:47 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: b4891eb03e6748ba870c61fd4ebd74e7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  14:51:47 up  4:43,  1 user,  load average: 0.49, 0.59, 0.66

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -170,10 +170,10 @@ void output_bmp(char *data,  struct myFILE *f){
   FILE *fp = fopen(f->filename, "wb+");
   if(!fp) return;
   //for(int i = f->position; i < f->next_sector; i ++)
-  int filesize = f->filesize;
-  while(filesize % 0x200) filesize++;
+  //int filesize = f->filesize;
+  //while(filesize % 0x200) filesize++;
   fwrite(data + (f->position - 0x2) * fat32.sector_size,
-    sizeof(char), filesize, fp);
+    sizeof(char), f->filesize, fp);
   fclose(fp);
 }
 
