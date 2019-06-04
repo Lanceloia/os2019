98dfe5b7c4c6258298c3f490076cdadb3ad9d9eb
tracer-nju <tracer@nju.edu.cn>
2019-06-04 10:32:06 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: b4891eb03e6748ba870c61fd4ebd74e7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  10:32:06 up 23 min,  1 user,  load average: 1.54, 2.62, 2.24

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -94,40 +94,31 @@ int tot_file;
 
 char buf[64][256] = {};
 
-int top, ctrl = 0;
+int top;
 
 void read_name_position_do(char *data, int offset, struct myFILE *file) {
-  switch(ctrl) {
-    case 0:
-      file->filename[0] = '\0';
-      if(*(data + offset + 0x0b) == (char)0x0f) {
-        read_unicode(buf[top], data + offset + 0x01, 5);
-        read_unicode(buf[top] + 5, data + offset + 0x0e, 6);
-        read_unicode(buf[top] + 11, data + offset + 0x1c, 2);
-        top ++;
-      }
-      else {
-        ctrl = 1;
-      }
-    break;
+  file->filename[0] = '\0';
+  if(*(data + offset + 0x0b) == (char)0x0f) {
+    read_unicode(buf[top], data + offset + 0x01, 5);
+    read_unicode(buf[top] + 5, data + offset + 0x0e, 6);
+    read_unicode(buf[top] + 11, data + offset + 0x1c, 2);
+    top ++;
+  }
+  else {
+    while(top --) {
+      strcat(file->filename, buf[top]);
+    }
 
-    case 1:
-      while(top --) {
-        strcat(file->filename, buf[top]);
-      }
-
-      file->position = read_num(data + offset + 0x14, 2) << 16;
-      file->position += read_num(data + offset + 0x1a, 2);
-      file->filesize = read_num(data + offset + 0x1c, 4);
-      file->next_sector = file->position + file->filesize / fat32.sector_size + 1; 
-      ctrl = 0;
-    break;
+    file->position = read_num(data + offset + 0x14, 2) << 16;
+    file->position += read_num(data + offset + 0x1a, 2);
+    file->filesize = read_num(data + offset + 0x1c, 4);
+    file->next_sector = file->position + file->filesize / fat32.sector_size + 1; 
   }
 }
 
 void read_name_position(char *data, int offset) {
-  if(ctrl == 0) {
-    tot_file ++, top = 0;
+  if(top == 0) {
+    tot_file ++;
     read_name_position_do(data, offset, &file[tot_file - 1]);
   }
   else {
@@ -142,65 +133,65 @@ void search_bmp_name_position(char *data, int offset) {
 }
 
 /*
-void search_bmp_head_structure(char *data, int offset) {
-  for(int j = 0; j < fat32.sector_size; j += 0x20) {
-    if(data[offset + j] == 'B' && data[offset + j + 1] == 'M') {
-      // init_BMP_INFO
-      BMP_INFO[tot_bmp].color = read_num(data + offset + j + 0x54, 3);
-      BMP_INFO[tot_bmp].width = read_num(data + offset + 0x12, 4);
-      BMP_INFO[tot_bmp].height = read_num(data + offset + 0x16, 4);
-      BMP_INFO[tot_bmp].offset = offset + j;
-      for(int i = 0; i < tot_file; i ++)
-        if (file[i].position == ((BMP_INFO[tot_bmp].offset - 0x81c00) / 0x200))
-          strcpy(BMP_INFO[tot_bmp].filename, file[i].filename);
-      tot_bmp ++;
-    }
-  }
+   void search_bmp_head_structure(char *data, int offset) {
+   for(int j = 0; j < fat32.sector_size; j += 0x20) {
+   if(data[offset + j] == 'B' && data[offset + j + 1] == 'M') {
+// init_BMP_INFO
+BMP_INFO[tot_bmp].color = read_num(data + offset + j + 0x54, 3);
+BMP_INFO[tot_bmp].width = read_num(data + offset + 0x12, 4);
+BMP_INFO[tot_bmp].height = read_num(data + offset + 0x16, 4);
+BMP_INFO[tot_bmp].offset = offset + j;
+for(int i = 0; i < tot_file; i ++)
+if (file[i].position == ((BMP_INFO[tot_bmp].offset - 0x81c00) / 0x200))
+strcpy(BMP_INFO[tot_bmp].filename, file[i].filename);
+tot_bmp ++;
 }
-*/
+}
+}
+ */
 
 void show_file(){
   for(int i = 0; i < tot_file; i ++){
     printf("%d: filename: %s, position: 0x%08x, filesize: 0x%08x, next_sector: 0x%08x\n",
-    i, file[i].filename, file[i].position, file[i].filesize, file[i].next_sector);
+            i, file[i].filename, file[i].position, file[i].filesize, file[i].next_sector);
   }
 }
 
 /*
-void show_BMP_INFO(){
-  for(int i = 0; i < tot_bmp; i ++) {
-    printf("bmp_index: %d, ", i);
-    printf("color: 0x%x, ", BMP_INFO[i].color);
-    printf("w: %d, h: %d, ", BMP_INFO[i].width, BMP_INFO[i].height);
-    printf("offset: 0x%x, ", BMP_INFO[i].offset);
-    printf("filename: %s, ", BMP_INFO[i].filename);
-  }
-}
-*/
+   void show_BMP_INFO(){
+   for(int i = 0; i < tot_bmp; i ++) {
+   printf("bmp_index: %d, ", i);
+   printf("color: 0x%x, ", BMP_INFO[i].color);
+   printf("w: %d, h: %d, ", BMP_INFO[i].width, BMP_INFO[i].height);
+   printf("offset: 0x%x, ", BMP_INFO[i].offset);
+   printf("filename: %s, ", BMP_INFO[i].filename);
+   }
+   }
+ */
 
 /*
-void output_bmp(char *data, struct BMP_INFO *yb){
-  FILE *fp = fopen(yb->filename, "wb+");
-  if(!fp) return;
-  printf("%x\n", *(int *)(data + yb->clusters[0]));
-  char head[54];
-  for(int i = 0; i < 54; i ++) {
-    head[i] = *(data + yb->clusters[0] + i);
-    // printf("%c", head[i]);
-  }
-  fwrite(head, sizeof(head), 1, fp);
-  char color[3];
-    color[0] = (yb->color) & 0xff;
-    color[1] = (yb->color >> 8) & 0xff;
-    color[2] = (yb->color >> 16) & 0xff;
-  int width = yb->width * 3;
-  while(width % 4 != 0) width ++;
-  for(int i = 0; i < yb->height; i ++)
-    for(int j = 0; j < width; j ++)
-      fwrite(color + (j % 3), sizeof(char), 1, fp);
-  fclose(fp);
+   void output_bmp(char *data, struct BMP_INFO *yb){
+   FILE *fp = fopen(yb->filename, "wb+");
+   if(!fp) return;
+   printf("%x\n", *(int *)(data + yb->clusters[0]));
+   char head[54];
+   for(int i = 0; i < 54; i ++) {
+   head[i] = *(data + yb->clusters[0] + i);
+// printf("%c", head[i]);
 }
-*/
+fwrite(head, sizeof(head), 1, fp);
+char color[3];
+color[0] = (yb->color) & 0xff;
+color[1] = (yb->color >> 8) & 0xff;
+color[2] = (yb->color >> 16) & 0xff;
+int width = yb->width * 3;
+while(width % 4 != 0) width ++;
+for(int i = 0; i < yb->height; i ++)
+for(int j = 0; j < width; j ++)
+fwrite(color + (j % 3), sizeof(char), 1, fp);
+fclose(fp);
+}
+ */
 
 int main(int argc, char *argv[]) {
   int fd = open(argv[1], O_RDWR);
@@ -210,7 +201,7 @@ int main(int argc, char *argv[]) {
   close(fd);
 
   read_fat32_info(imgmap);
-  
+
   for(int i = 0; i < 32 MB; i += fat32.sector_size) {
     search_bmp_name_position(imgmap, i);
   }
@@ -219,8 +210,8 @@ int main(int argc, char *argv[]) {
   //show_BMP_INFO();
 
   /*
-  for(int i = 0; i < tot_bmp; i ++)
-    output2_bmp(imgmap, &BMP_INFO[i]);
-  */
+     for(int i = 0; i < tot_bmp; i ++)
+     output2_bmp(imgmap, &BMP_INFO[i]);
+   */
   return 0;
 }
