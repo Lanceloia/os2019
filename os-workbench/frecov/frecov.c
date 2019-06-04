1a352de7bd0b8fff0aac7c527999a8acd75fbc05
tracer-nju <tracer@nju.edu.cn>
2019-06-04 16:11:58 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: b4891eb03e6748ba870c61fd4ebd74e7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  16:11:58 up  6:03,  1 user,  load average: 1.44, 1.24, 1.03

--- os-workbench/frecov/frecov.c
+++ os-workbench/frecov/frecov.c
@@ -110,7 +110,9 @@ int is_valid(int key){
 }
 
 void read_name_position_do(char *data, int offset, struct myFILE *file) {
-  if(*(data + offset + 0x0b) == (char)0x0f && is_valid(read_num(data + offset, 1))) {
+  if(*(data + offset + 0x0b) == (char)0x0f &&
+    *(data + offset + 0x0 ) == (char)0x00 &&
+   is_valid(read_num(data + offset, 1))) {
     read_unicode(buf[top], data + offset + 0x01, 5);
     read_unicode(buf[top] + 5, data + offset + 0x0e, 6);
     read_unicode(buf[top] + 11, data + offset + 0x1c, 2);
