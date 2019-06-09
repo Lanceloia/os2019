8b036938842597f79315c7e3b270f4a8f9a5b10b
tracer-nju <tracer@nju.edu.cn>
2019-06-09 18:41:30 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: e1bda2c847724f4684b269b43e59310c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  18:41:30 up  6:20,  1 user,  load average: 0.34, 0.55, 0.72

--- os-workbench/kernel/include/fs.h
+++ os-workbench/kernel/include/fs.h
@@ -1,9 +1,57 @@
 #ifndef __FS_H__
 #define __FS_H__
 
-enum {
+enum
+{
     RD_ENABLE = 1,
     WR_ENABLE = 2,
+};
+
+typedef struct file file_t;
+typedef struct inodeops inodeops_t;
+typedef struct inode inode_t;
+typedef struct filesystem filesystem_t;
+typedef struct fsops fsops_t;
+
+struct file
+{
+    int refcnt;
+    inode_t *inode;
+    uint64_t offset;
+};
+
+struct inode
+{
+    int refcnt;
+    void *prt; //??
+    filesystem_t *fs;
+    inodeops_t *ops;
+};
+
+struct inodeops
+{
+    int (*open)(file_t *file, int flags);
+    int (*close)(file_t *file);
+    ssize_t (*read)(file_t *file, char *buf, size_t size);
+    ssize_t (*write)(file_t *file, const char *buf, size_t size);
+    off_t (*lseek)(file_t *file, off_t offset, int whence);
+    int (*mkdir)(const char *name);
+    int (*rmdir)(const char *name);
+    int (*link)(const char *name, inode_t *inode);
+    int (*unlink)(const char *name);
+};
+
+struct filesystem
+{
+    char name[NAME_lENGTH];
+    device_t *dev;
+};
+
+struct fsop
+{
+    void (*init)(filesystem_t *fs, const char *name, device_t *dev);
+    inode_t *(lookup)(filesystem_t *fs, const char *path, int flags);
+    int (*close)(inode_t *inode);
 };
 
 #endif
