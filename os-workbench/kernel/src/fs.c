9e04dde4a90c616c97803333f968bbfeaadaac4b
tracer-nju <tracer@nju.edu.cn>
2019-06-09 16:41:47 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: e1bda2c847724f4684b269b43e59310c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  16:41:47 up  4:20,  1 user,  load average: 1.30, 0.82, 0.83

---
+++ os-workbench/kernel/src/fs.c
@@ -0,0 +1,15 @@
+MODULE_DEF(kmt) {
+  .init = kmt_init,
+    .create = kmt_create,
+    .teardown = kmt_teardown,
+    .spin_init = kmt_spin_init,
+    .spin_lock = kmt_spin_lock,
+    .spin_unlock = kmt_spin_unlock,
+    .sem_init = kmt_sem_init,
+    .sem_wait = kmt_sem_wait,
+    .sem_signal = kmt_sem_signal,
+};
+
+#ifdef _LANCELOIA_DEBUG_
+#undef _LANCELOIA_DEBUG_
+#endif
