6d44d97c8df0847b1c6354ee3db3fe6806566011
tracer-nju <tracer@nju.edu.cn>
2019-06-09 18:09:51 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: e1bda2c847724f4684b269b43e59310c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  18:09:51 up  5:48,  1 user,  load average: 0.78, 0.85, 1.00

--- os-workbench/kernel/src/devices/tty/tty.c
+++ os-workbench/kernel/src/devices/tty/tty.c
@@ -338,24 +338,38 @@ void tty_task(void *arg)
 }
 
 char readbuf[128], writebuf[128];
+char bigbuf[2048] = {};
 
 /* shell command */
 #include <fs.h>
 
 static void echo_do(char *str)
 {
-    sprintf(writebuf, "%s\n", str);
+  sprintf(bigbuf, "%s\n", str);
+  tty->ops->write(tty, 0, bigbuf, strlen(bigbuf));
 }
 
 static void cat_do(char *path)
 {
-  vfs->open(path, RD_ENABLE);
-  sprintf(writebuf, "not fulfil\n");
+  int fd = vfs->open(path, RD_ENABLE);
+  if (fd == -1)
+  {
+    panic("error");
+    return;
+  }
+  while (vfs->read(fd, bigbuf, 1024))
+    tty->ops->write(tty, 0, bigbuf, strlen(bigbuf));
+  if (vfs->close(fd))
+  {
+    panic("error");
+    return;
+  }
 }
 
 static void default_do()
 {
-  sprintf(writebuf, "unexpected command\n");
+  sprintf(bigbuf, "unexpected command\n");
+  tty->ops->write(tty, 0, bigbuf, strlen(bigbuf));
 }
 
 void shell_task(void *name)
@@ -368,12 +382,11 @@ void shell_task(void *name)
     int nread = tty->ops->read(tty, 0, readbuf, sizeof(readbuf));
     readbuf[nread - 1] = '\0';
 
-    if(strncmp(readbuf, "echo ", 5) == 0)
+    if (strncmp(readbuf, "echo ", 5) == 0)
       echo_do(readbuf + 5);
-    else if(strncmp(readbuf, "cat ", 4) == 0)
+    else if (strncmp(readbuf, "cat ", 4) == 0)
       cat_do(readbuf + 4);
     else
       default_do();
-    tty->ops->write(tty, 0, writebuf, strlen(writebuf));
   }
 }
