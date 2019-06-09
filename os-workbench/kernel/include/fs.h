a0616ce8df0381cbc1aa24c9bfc8469781e9ecf6
tracer-nju <tracer@nju.edu.cn>
2019-06-09 18:11:55 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: e1bda2c847724f4684b269b43e59310c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  18:11:55 up  5:50,  1 user,  load average: 1.66, 1.02, 1.03

--- os-workbench/kernel/include/fs.h
+++ os-workbench/kernel/include/fs.h
@@ -6,17 +6,4 @@ enum {
     WR_ENABLE = 2,
 };
 
-static int vfs_access(const char *path, int mode);
-static int vfs_mount(const char *path, filesystem_t *fs);
-static int vfs_unmount(const char *path);
-static int vfs_mkdir(const char *path);
-static int vfs_rmdir(const char *path);
-static int vfs_link(const char *oldpath, const char *newpath);
-static int vfs_unlink(const char *path);
-static int vfs_open(const char *path, int flags);
-static ssize_t vfs_read(int fd, void *buf, size_t nbyte);
-static ssize_t vfs_write(int fd, void *buf, size_t nbyte);
-static off_t vfs_lseek(int fd, off_t offset, int whence);
-static int vfs_close(int fd);
-
 #endif
