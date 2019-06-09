16ac1fd83fe4631bfb173a11280eeaa44719164b
tracer-nju <tracer@nju.edu.cn>
2019-06-09 17:04:14 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: e1bda2c847724f4684b269b43e59310c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-20-generic       Architecture: x86-64  17:04:15 up  4:43,  1 user,  load average: 1.78, 2.20, 1.85

--- os-workbench/kernel/src/fs.c
+++ os-workbench/kernel/src/fs.c
@@ -2,35 +2,82 @@
 #include <klib.h>
 #include <fs.h>
 
-void vfs_init(){}
-int vfs_access(const char *path, int mode){}
-int vfs_mount(const char *path, filesystem_t *fs){}
-int vfs_unmount(const char *path){}
-int vfs_mkdir(const char *path){}
-int vfs_rmdir(const char *path){}
-int vfs_link(const char *oldpath, const char *newpath){}
-int vfs_unlink(const char *path){}
-int vfs_open(const char *path, int flags){}
-ssize_t vfs_read(int fd, void *buf, size_t nbyte){}
-ssize_t vfs_write(int fd, void *buf, size_t nbyte){}
-off_t vfs_lseek(int fd, off_t offset, int whence){}
-int vfs_close(int fd){}
+void vfs_init() {}
 
-MODULE_DEF(vfs)
+int vfs_access(const char *path, int mode)
 {
-  .init = vfs_init,
-  .access = vfs_access,
-  .mount = vfs_mount,
-  .unmount = vfs_unmount,
-  .mkdir = vfs_mkdir,
-  .rmdir = vfs_rmdir,
-  .link = vfs_link,
-  .unlink = vfs_unlink,
-  .open = vfs_open,
-  .read = vfs_read,
-  .write = vfs_write,
-  .lseek = vfs_lseek,
-  .close = vfs_close,
+    return 1;
+}
+
+int vfs_mount(const char *path, filesystem_t *fs)
+{
+    return 1;
+}
+
+int vfs_unmount(const char *path)
+{
+    return 1;
+}
+
+int vfs_mkdir(const char *path)
+{
+    return 1;
+}
+
+int vfs_rmdir(const char *path)
+{
+    return 1;
+}
+
+int vfs_link(const char *oldpath, const char *newpath)
+{
+    return 1;
+}
+
+int vfs_unlink(const char *path)
+{
+    return 1;
+}
+
+int vfs_open(const char *path, int flags)
+{
+    return 1;
+}
+
+ssize_t vfs_read(int fd, void *buf, size_t nbyte)
+{
+    return 1;
+}
+
+ssize_t vfs_write(int fd, void *buf, size_t nbyte)
+{
+    return 1;
+}
+
+off_t vfs_lseek(int fd, off_t offset, int whence)
+{
+    return 1;
+}
+
+int vfs_close(int fd)
+{
+    return 1;
+}
+
+MODULE_DEF(vfs){
+    .init = vfs_init,
+    .access = vfs_access,
+    .mount = vfs_mount,
+    .unmount = vfs_unmount,
+    .mkdir = vfs_mkdir,
+    .rmdir = vfs_rmdir,
+    .link = vfs_link,
+    .unlink = vfs_unlink,
+    .open = vfs_open,
+    .read = vfs_read,
+    .write = vfs_write,
+    .lseek = vfs_lseek,
+    .close = vfs_close,
 };
 
 #ifdef _LANCELOIA_DEBUG_
