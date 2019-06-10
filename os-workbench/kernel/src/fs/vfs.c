5c8a25b4d702a34b0020f6c05c416243201189c7
tracer-nju <tracer@nju.edu.cn>
2019-06-11 04:03:20 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 053c91a0e5274d99adeb83f0cd2b6b85   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  04:03:20 up 11:25,  1 user,  load average: 1.66, 1.17, 1.05

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -107,7 +107,8 @@ int vfs_open(const char *path, int flags) {
   int fd = alloc_fd();
   if (fd == -1) return 1;
   int idx = identify_fs(path);
-  fds[fd].id = _fs[idx].ops->lookup(&_fs[idx], path + strlen(_path[idx]), mode);
+  fds[fd].id =
+      _fs[idx].ops->lookup(&_fs[idx], path + strlen(_path[idx]), flags);
   fds[fd].offset = 0;
   fds[fd].refcnt++;
   return 0;
