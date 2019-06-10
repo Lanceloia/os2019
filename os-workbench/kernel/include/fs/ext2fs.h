2fbf3a2267d6da4aa05a8ad7af874bf5ba91cdcd
tracer-nju <tracer@nju.edu.cn>
2019-06-11 03:08:41 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 053c91a0e5274d99adeb83f0cd2b6b85   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  03:08:41 up 10:30,  1 user,  load average: 1.49, 1.20, 0.90

---
+++ os-workbench/kernel/include/fs/ext2fs.h
@@ -0,0 +1,111 @@
+#ifndef __SIMEXT2_H__
+#define __SIMEXT2_H__
+
+#include <devices.h>
+
+#define BLK_SIZE (512)
+#define DISK_START (0 * BLK_SIZE)          // disk offset
+#define GDT_START (1 * BLK_SIZE)           // group_desc table offset
+#define BLK_BITMAP (2 * BLK_SIZE)          // block bitmap offset
+#define IND_BITMAP (3 * BLK_SIZE)          // inode bitmap offset
+#define INDT_START (4 * BLK_SIZE)          // inode table offset
+#define DATA_BLOCK ((4 + 512) * BLK_SIZE)  // data block offset
+#define DATA_SIZE (4096)                   // data block size(blocks)
+#define DISK_SIZE (DATA_SIZE + 512)        // disk size(blocks)
+#define EXT2_N_BLOCKS (12)                 // ext2 inode blocks
+#define VOLUME_NAME "EXT2FS"               // volume name
+
+struct super_block {
+  /* super block, 32 bytes */
+  char volume_name[16];
+  uint16_t disk_size;  // maybe have no use
+  uint16_t blocks_per_group;
+  uint16_t inodes_per_blocks;
+  char pad[10];
+};
+
+#define INODE_TABLE_COUNT 4096
+#define DATA_BLOCK_COUNT 4096
+
+struct group_desc {
+  /* block group descriptor, 32 bytes */
+  char volume_name[16];
+  uint16_t block_bitmap;       // the blk idx of data blk-bitmap
+  uint16_t inode_bitmap;       // the blk idx of inode-bitmap
+  uint16_t inode_table;        // the blk idx of inode-table
+  uint16_t free_blocks_count;  // the amount of free blks
+  uint16_t free_inodes_count;  // the amount of free inodes
+  uint16_t used_dirs_count;    // the amount of dirs
+  char pad[4];
+};
+
+struct inode {
+  /* inode, 64 bytes */
+  uint16_t mode;                  // the mode of file
+  uint32_t blocks;                // the size of file (blks)
+  uint32_t size;                  // the size of file (bytes)
+  uint32_t block[EXT2_N_BLOCKS];  // direct or indirect blocks
+  char pad[6];
+};
+
+struct directory {
+  /* directory entry, 32 bytes */
+  uint32_t inode;
+  uint16_t rec_len;
+  uint8_t name_len;
+  uint8_t file_type;
+  char name[16];
+  char pad[8];
+};
+
+typedef struct super_block sb_t;
+typedef struct group_desc gd_t;
+typedef struct inode ind_t;
+typedef struct directory dir_t;
+
+#define SB_SIZE (sizeof(sb_t))
+#define GD_SIZE (sizeof(gd_t))
+#define IND_SIZE (sizeof(ind_t))
+#define DIR_SIZE (sizeof(dir_t))
+#define DIR_AMUT (BLK_SIZE / DIR_SIZE)
+#define MAX_OPEN_FILE_AMUT (16)
+
+struct ext2 {
+  struct super_block sb;
+  struct group_desc gdt;
+  struct inode ind;
+  struct directory dir[DIR_AMUT];
+  unsigned char blockbitmapbuf[BLK_SIZE];
+  unsigned char inodebitmapbuf[BLK_SIZE];
+  unsigned char datablockbuf[BLK_SIZE];
+  uint32_t last_alloc_block;
+  uint32_t last_alloc_inode;
+  uint32_t current_dir;
+  uint32_t current_dir_name_len;
+  uint32_t file_open_table[MAX_OPEN_FILE_AMUT];
+  char current_dir_name[256];
+  device_t* dev;
+};
+
+typedef struct ext2 ext2_t;
+
+enum { TYPE_FILE = 1, TYPE_DIR = 2 };
+
+void ext2_rd_sb(ext2_t* ext2);
+void ext2_wr_sb(ext2_t* ext2);
+void ext2_rd_gd(ext2_t* ext2);
+void ext2_wr_gd(ext2_t* ext2);
+void ext2_rd_ind(ext2_t* ext2, uint32_t i);
+void ext2_wr_ind(ext2_t* ext2, uint32_t i);
+void ext2_rd_dir(ext2_t* ext2, uint32_t i);
+void ext2_wr_dir(ext2_t* ext2, uint32_t i);
+void ext2_rd_blockbitmap(ext2_t* ext2);
+void ext2_wr_blockbitmap(ext2_t* ext2);
+void ext2_rd_inodebitmap(ext2_t* ext2);
+void ext2_wr_inodebitmap(ext2_t* ext2);
+void ext2_rd_datablock(ext2_t* ext2, uint32_t i);
+void ext2_wr_datablock(ext2_t* ext2, uint32_t i);
+
+void ext2_ls(ext2_t* ext2, char* dirname, char* out);
+
+#endif
---
+++ os-workbench/kernel/src/fs/vfs.c
@@ -0,0 +1,125 @@
+#include <fs.h>
+#include <klib.h>
+#include <simext2.h>
+
+#define MAX_FS 4
+
+fs_t _fs[MAX_FS];
+fs_ops_t _fs_ops[MAX_FS];
+
+typedef struct device device_t;
+extern device_t *dev_lookup(const char *name);
+
+typedef struct ext2 ext2_t;
+extern void ext2_init(fs_t *fs, const char *name, device_t *dev);
+extern id_t *ext2_lookup(fs_t *fs, const char *path, int flags);
+extern int ext2_close(id_t *id);
+
+void vfs_build(int idx, char *name, device_t *dev, size_t size,
+               void (*init)(fs_t *, const char *, device_t *),
+               id_t *(*lookup)(fs_t *fs, const char *path, int flags),
+               int (*close)(id_t *id)) {
+  strcpy(_fs[idx].name, name);
+  // printf("name: %s", name);
+  _fs[idx].fs = pmm->alloc(size);
+  _fs[idx].ops = &_fs_ops[idx];
+  _fs[idx].dev = dev;
+  _fs_ops[idx].init = init;
+  _fs_ops[idx].lookup = lookup;
+  _fs_ops[idx].close = close;
+  _fs_ops[idx].init(&_fs[idx], name, dev);
+}
+
+void vfs_init() {
+  vfs_build(0, "ext2fs-ramdisk0", dev_lookup("ramdisk0"), sizeof(ext2_t),
+            ext2_init, ext2_lookup, ext2_close);
+  // vfs_build(1, "ext2fs-ramdisk1", dev_lookup("ramdisk1"));
+}
+
+int vfs_access(const char *path, int mode) {
+  assert(0);
+  return 0;
+}
+
+int vfs_mount(const char *path, fs_t *fs) {
+  assert(0);
+  return 0;
+}
+
+int vfs_unmount(const char *path) {
+  assert(0);
+  return 1;
+}
+
+int vfs_mkdir(const char *path) {
+  assert(0);
+  return 1;
+}
+
+int vfs_rmdir(const char *path) {
+  assert(0);
+  return 1;
+}
+
+int vfs_link(const char *oldpath, const char *newpath) {
+  assert(0);
+  return 1;
+}
+
+int vfs_unlink(const char *path) {
+  assert(0);
+  return 1;
+}
+
+int vfs_open(const char *path, int flags) {
+  assert(0);
+  return 1;
+}
+
+ssize_t vfs_read(int fd, void *buf, size_t nbyte) {
+  assert(0);
+  return 1;
+}
+
+ssize_t vfs_write(int fd, void *buf, size_t nbyte) {
+  assert(0);
+  return 1;
+}
+
+off_t vfs_lseek(int fd, off_t offset, int whence) {
+  assert(0);
+  return 1;
+}
+
+int vfs_close(int fd) {
+  assert(0);
+  return 1;
+}
+
+fs_t *vfs_get_fs(int idx) {
+  if (idx < 0 || idx >= MAX_FS)
+    return NULL;
+  else
+    return &_fs[idx];
+}
+
+MODULE_DEF(vfs){
+    .init = vfs_init,
+    .access = vfs_access,
+    .mount = vfs_mount,
+    .unmount = vfs_unmount,
+    .mkdir = vfs_mkdir,
+    .rmdir = vfs_rmdir,
+    .link = vfs_link,
+    .unlink = vfs_unlink,
+    .open = vfs_open,
+    .read = vfs_read,
+    .write = vfs_write,
+    .lseek = vfs_lseek,
+    .close = vfs_close,
+    .get_fs = vfs_get_fs,
+};
+
+#ifdef _LANCELOIA_DEBUG_
+#undef _LANCELOIA_DEBUG_
+#endif
