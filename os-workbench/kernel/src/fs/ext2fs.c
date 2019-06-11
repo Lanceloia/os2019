1747c3801c3aaae575f227276469043f67b42be5
tracer-nju <tracer@nju.edu.cn>
2019-06-11 22:23:35 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 0350bf5f137d490885bcb69403c8ec07   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  22:23:35 up 40 min,  1 user,  load average: 1.20, 1.09, 0.90

--- os-workbench/kernel/src/fs/ext2fs.c
+++ os-workbench/kernel/src/fs/ext2fs.c
@@ -120,25 +120,24 @@ uint32_t ext2_alloc_inode(ext2_t* ext2) {
   return ext2->last_alloc_inode;
 }
 
-uint32_t ext2_reserch_file(ext2_t* ext2, char* path, int file_type,
-                           uint32_t* ninode, uint32_t* nblock, uint32_t* ndir) {
+uint32_t ext2_reserch_file(ext2_t* ext2, char* path, int type, uint32_t* ninode,
+                           uint32_t* nblock, uint32_t* ndir) {
   ext2_rd_ind(ext2, ext2->current_dir);
   for (uint32_t j = 0; j < ext2->ind.blocks; j++) {
     ext2_rd_dir(ext2, ext2->ind.block[j]);
     int len = first_item_len(path);
     for (uint32_t k = 0; k < DIR_AMUT;) {
-      if (!ext2->dir[k].inode || ext2->dir[k].file_type != file_type ||
+      if (!ext2->dir[k].inode || ext2->dir[k].file_type != type ||
           strncmp(ext2->dir[k].name, path, len)) {
         k++;
-      } else if (len < strlen(path)) {
-        ext2->current_dir = ext2->dir[k].inode;
-        return ext2_reserch_file(ext2, path + len + 1, file_type, ninode,
-                                 nblock, ndir);
       } else {
-        *ninode = ext2->dir[k].inode;
+        ext2->current_dir = *ninode = ext2->dir[k].inode;
         *nblock = j;
         *ndir = k;
-        return 1;
+        return (len == strlen(path))
+                   ? 1
+                   : ext2_reserch_file(ext2, path + len + 1, type, ninode,
+                                       nblock, ndir);
       }
     }
   }
