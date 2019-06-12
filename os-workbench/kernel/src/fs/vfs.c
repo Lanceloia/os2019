f7080f1eced50cfdbb2cf68cc0f97a64ce6c46e3
tracer-nju <tracer@nju.edu.cn>
2019-06-13 01:54:19 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 230bd2cfe7e44eca9520c40c0ed484af   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  01:54:19 up 10:23,  1 user,  load average: 1.14, 1.04, 1.01

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -73,7 +73,7 @@ static int lookup_auto(char *path) {
   while (vinodes[idx].fs->readdir(vinodes[idx].fs, vinodes[idx].rinode_idx,
                                   ++kth, &vinodes[newidx].rinode_idx,
                                   vinodes[newidx].name))
-    printf("newidx: %d, rinode_idx: %s, name %s\n", newidx,
+    printf("newidx: %d, rinode_idx: %d, name %s\n", newidx,
            vinodes[newidx].rinode_idx, vinodes[newidx].name);
   return lookup_auto(path);
 }
