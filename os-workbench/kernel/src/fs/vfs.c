4fe825aef0f0867dd437ad945053d634b99c7224
tracer-nju <tracer@nju.edu.cn>
2019-06-12 21:35:51 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 230bd2cfe7e44eca9520c40c0ed484af   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  21:35:51 up  6:04,  1 user,  load average: 0.91, 1.13, 1.01

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -3,18 +3,17 @@
 #include "../../include/fs/ext2fs.h"
 #include "../../include/fs/vfs.h"
 
-#define MAX_FS 4
+#define MAX_FILESYSTEM 16
 
-fs_t _fs[MAX_FS];
-fs_ops_t _fs_ops[MAX_FS];
-char *_path[MAX_FS];
+filesystem_t filesys[MAX_FS];
 
 typedef struct device device_t;
 extern device_t *dev_lookup(const char *name);
 
+/*
 typedef struct ext2 ext2_t;
 extern void ext2_init(fs_t *fs, const char *name, device_t *dev);
-/*
+
 extern id_t *ext2_lookup(fs_t *fs, const char *path, int flags);
 extern int ext2_open(id_t *id, int flags);
 extern int ext2_close(id_t *id);
@@ -22,12 +21,16 @@ extern int ext2_mkdir(const char *dirname);
 extern int ext2_rmdir(const char *dirname);
 */
 
+/*
+
 id_t *ext2_lookup_tmp(fs_t *fs, const char *path, int flags) { return NULL; }
 int ext2_open_tmp(id_t *id, int flags) { return 0; }
 int ext2_close_tmp(id_t *id) { return 0; }
 int ext2_mkdir_tmp(const char *dirname) { return 0; }
 int ext2_rmdir_tmp(const char *dirname) { return 0; }
+*/
 
+/*
 void vfs_device(int idx, char *name, device_t *dev, size_t size,
                 void (*init)(fs_t *, const char *, device_t *),
                 id_t *(*lookup)(fs_t *fs, const char *path, int flags),
@@ -47,18 +50,9 @@ void vfs_device(int idx, char *name, device_t *dev, size_t size,
   _fs_ops[idx].rmdir = rmdir;
   _fs_ops[idx].init(&_fs[idx], name, dev);
 }
+*/
 
-#define MAX_DIRS 256
-
-struct vfsdir {
-  char name[16];
-  char absolutely_name[256];
-  int dot, ddot;
-  int next, child, type;
-  void *real_fs;
-} vfsdirs[MAX_DIRS] = {};
-int cur_dir, dev_dir, proc_dir;
-
+/*
 int vfsdirs_alloc(const char *name, int parent, int type, int fs_idx) {
   int idx = -1;
   for (int i = 0; idx == -1 && i < MAX_DIRS; i++)
@@ -90,10 +84,10 @@ int vfsdirs_alloc(const char *name, int parent, int type, int fs_idx) {
   }
   return idx;
 }
-
-int total_dev_cnt = 0;
+*/
 
 void vfs_init() {
+  /*
   // vfs_device(1, "tty1", dev_lookup("tty1"));
   cur_dir = 0;
   strcpy(vfsdirs[0].name, "/");
@@ -112,151 +106,32 @@ void vfs_init() {
              ext2_init, ext2_lookup_tmp, ext2_open_tmp, ext2_close_tmp,
              ext2_mkdir_tmp, ext2_rmdir_tmp);
   vfsdirs_alloc("ramdisk1", dev_dir, EXT2, total_dev_cnt++);
-}
-
-/*
-void vfsdirs_dfs(const char *path){
-
-}
-*/
-
-int vfs_match_path(const char *path) {
-  int idx = -1, len = 0;
-  for (int i = 0; i < MAX_DIRS; i++) {
-    int slen = strlen(vfsdirs[i].absolutely_name);
-    if (!strncmp(path, vfsdirs[i].absolutely_name, slen))
-      if (slen > len) {
-        idx = i;
-        len = slen;
-      }
-  }
-  return idx;
-}
-
-int vfs_identify_fs(const char *path) {
-  int idx = vfs_match_path(path);
-  if (idx == -1) {
-    printf("unknown filesystem\n");
-    return 0;
-  }
-  int type = vfsdirs[idx].type;
-  if (!strcmp(path, vfsdirs[idx].absolutely_name))  // absolutely_equal
-    return type | INTERFACE;
-  else
-    return type;
-}
-
-void *vfs_get_real_fs(const char *path) {
-  int idx = vfs_match_path(path);
-  if (idx == -1) {
-    printf("unknown filesystem\n");
-    return NULL;
-  }
-  // printf("idx: %d\n", idx);
-  return vfsdirs[idx].real_fs;
-}
-
-int vfs_access(const char *path, int mode) {
-  /*
-  int idx = vfs_identify_fs(path);
-  // 0 for accessable
-  if (_fs[idx].ops->lookup(&_fs[idx], path + strlen(_path[idx]), mode) ==
-  NULL) return 1;
   */
-  return 0;
 }
 
-int vfs_mount(const char *path, fs_t *fs) {
-  /*
-  int idx = fs - &_fs[0];
-  if (idx < 0 || idx >= MAX_FS) return 1;
-  strcpy(_path[idx], path);
-  */
-  return 0;
-}
+int vfs_access(const char *path, int mode) { return 0; }
 
-int vfs_unmount(const char *path) {
-  /*
-  int idx = vfs_identify_fs(path);
-  _path[idx] = "DISABLE_PATH";
-  */
-  return 0;
-}
+int vfs_mount(const char *path, filesystem_t *fs) { return 0; }
 
-int vfs_mkdir(const char *path) {
-  assert(0);
-  return 1;
-}
+int vfs_unmount(const char *path) { return 0; }
 
-int vfs_rmdir(const char *path) {
-  assert(0);
-  return 1;
-}
+int vfs_mkdir(const char *path) { return 0; }
 
-int vfs_link(const char *oldpath, const char *newpath) {
-  assert(0);
-  return 1;
-}
+int vfs_rmdir(const char *path) { return 0; }
 
-int vfs_unlink(const char *path) {
-  assert(0);
-  return 1;
-}
+int vfs_link(const char *oldpath, const char *newpath) { return 0; }
 
-/*
-static int alloc_fd() {
-  for (int i = 0; i < MAX_FD; i++)
-    if (fds[i].refcnt == 0) return i;
-  return -1;
-}
-*/
+int vfs_unlink(const char *path) { return 0; }
 
-int vfs_open(const char *path, int flags) {
-  /*
-  assert(0);
-  if (vfs_access(path, flags)) return 1;
-  int fd = alloc_fd();
-  if (fd == -1) return 1;
-  int idx = vfs_identify_fs(path);
-  fds[fd].id =
-      _fs[idx].ops->lookup(&_fs[idx], path + strlen(_path[idx]), flags);
-  fds[fd].offset = 0;
-  fds[fd].refcnt++;
-  */
-  return 0;
-}
+int vfs_open(const char *path, int flags) { return 0; }
 
-ssize_t vfs_read(int fd, void *buf, size_t nbyte) {
-  /*
-  ssize_t cnt = fds[fd].id->ops->read(&fds[fd], buf, nbyte);
-  fds[fd].offset += cnt;
-  */
-  return 0;
-}
+ssize_t vfs_read(int fd, void *buf, size_t nbyte) { return 0; }
 
-ssize_t vfs_write(int fd, void *buf, size_t nbyte) {
-  /*
-  assert(0);
-  ssize_t cnt = fds[fd].id->ops->write(&fds[fd], buf, nbyte);
-  fds[fd].offset += cnt;
-  */
-  return 0;
-}
+ssize_t vfs_write(int fd, void *buf, size_t nbyte) { return 0; }
 
-off_t vfs_lseek(int fd, off_t offset, int whence) {
-  assert(0);
-  return 1;
-}
+off_t vfs_lseek(int fd, off_t offset, int whence) { return 0; }
 
-int vfs_close(int fd) {
-  /*
-  if (!(--fds[fd].refcnt)) {
-    fds[fd].id = NULL;
-    fds[fd].offset = 0;
-  }
-  */
-  return 0;
-}
+int vfs_close(int fd) { return 0; }
 
 void vfs_cd(char *dirname, char *pwd, char *out) {
   int offset = sprintf(out, "");
@@ -319,3 +194,5 @@ MODULE_DEF(vfs){
 #ifdef _LANCELOIA_DEBUG_
 #undef _LANCELOIA_DEBUG_
 #endif
+
+* /
