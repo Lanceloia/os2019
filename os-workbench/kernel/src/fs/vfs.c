40358313c29eb1803e38776cf8f36fcaae2f7726
tracer-nju <tracer@nju.edu.cn>
2019-06-13 00:17:01 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 230bd2cfe7e44eca9520c40c0ed484af   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  00:17:01 up  8:45,  1 user,  load average: 1.33, 0.91, 0.77

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -170,7 +170,7 @@ off_t vfs_lseek(int fd, off_t offset, int whence) { return 0; }
 int vfs_close(int fd) { return 0; }
 
 void vfs_ls(int idx) {
-  for (int k = vinodes[idx].child, k != -1; k = vinodes[k].next) {
+  for (int k = vinodes[idx].child; k != -1; k = vinodes[k].next) {
     printf("id:%-8d    name:%-8s\n", k, vinodes[k].name);
   }
 }
