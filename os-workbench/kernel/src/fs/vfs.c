20b3535a1129a67f67055fa8b75e084147e8af1f
tracer-nju <tracer@nju.edu.cn>
2019-06-13 10:08:58 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 5ac9696ac40d4753823d1df02ef1a975   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  10:08:58 up  7:21,  1 user,  load average: 1.16, 0.79, 0.73

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -69,7 +69,7 @@ static int lookup_cur(char *path, int *pflag, int cur) {
     next = vinodes[next].next_link;
   }
 
-  char *newpath = path + strlen(vinodes[k].name);
+  char *newpath = path + len + 1;
   printf("old: %s, new: %s, name: %s, next: %d\n", path, newpath,
          vinodes[k].name, next);
   return lookup_cur(newpath, pflag, next);
