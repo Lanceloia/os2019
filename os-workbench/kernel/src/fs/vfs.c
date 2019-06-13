18eb6f37a216173b510a121d2b5ff7c8652f6c9a
tracer-nju <tracer@nju.edu.cn>
2019-06-13 18:05:56 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 5ac9696ac40d4753823d1df02ef1a975   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  18:05:56 up 15:18,  1 user,  load average: 1.07, 0.85, 0.97

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -44,7 +44,7 @@ static int first_item_len(const char *path) {
 }
 
 static int item_match(const char *P, const char *T, int len) {
-  printf("P: %s\nT: %s\nlen: %d\n\n");
+  printf("P: %s\nT: %s\nlen: %d\n\n", P, T, len);
   if (strncmp(P, T, len))
     return 0;
   else
