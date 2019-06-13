b02f67f7c418c1882b048e43a75f36fdd7b33bc1
tracer-nju <tracer@nju.edu.cn>
2019-06-13 19:06:41 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 5ac9696ac40d4753823d1df02ef1a975   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  19:06:41 up 16:19,  1 user,  load average: 1.21, 1.09, 1.12

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -374,7 +374,7 @@ ssize_t vfs_read(int fd, char *buf, size_t nbyte) {
   assert(nbyte <= 1024);
   extern ssize_t ext2_read(ext2_t * ext2, int rinode_idx, char *buf,
                            uint32_t len);
-  return ext2_read(vinodes[files[fd].vinode_idx].fs->rfs, bufm len);
+  return ext2_read(vinodes[files[fd].vinode_idx].fs->rfs, buf len);
 }
 
 ssize_t vfs_write(int fd, char *buf, size_t nbyte) { return 0; }
