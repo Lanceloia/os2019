54050468d4f1dddf7dfc3fa5bb2246597effad4f
tracer-nju <tracer@nju.edu.cn>
2019-06-13 23:48:10 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: d185befb6bf54cab92a1a0e80eeff62c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  23:48:10 up 28 min,  1 user,  load average: 0.99, 1.00, 0.86

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -356,7 +356,7 @@ int vfs_init() {
   int dev = vinodes_append_dir(root, "dev", VFS, NULL);
 
   // printf("fuck");
-  vinodes_create_dir(dev, root, NULL);
+  vinodes_create_dir(dev, root, VFS, NULL);
 
   int fs_r0 = vfs_init_devfs("ramdisk0", dev_lookup("ramdisk0"), sizeof(ext2_t),
                              ext2_init, ext2_lookup, ext2_readdir);
