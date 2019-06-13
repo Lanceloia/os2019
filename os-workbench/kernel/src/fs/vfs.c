9e5b1fe37943d77ee73c70ee42e4b7abf4c6ccad
tracer-nju <tracer@nju.edu.cn>
2019-06-13 09:02:24 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 5ac9696ac40d4753823d1df02ef1a975   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  09:02:25 up  6:14,  1 user,  load average: 0.86, 1.04, 1.08

--- os-workbench/kernel/src/fs/vfs.c
+++ os-workbench/kernel/src/fs/vfs.c
@@ -89,6 +89,7 @@ static int lookup_auto(char *path) {
   while ((ret = pidx->fs->readdir(pidx->fs, pidx->rinode_idx, ++kth, &buf))) {
     if ((nidx = vinodes_alloc()) == -1) assert(0);
     vinodes[nidx] = buf;
+    vinodes[nidx].child = -1;
     vinodes[nidx].next = -1;
     if (oidx == -1)
       pidx->child = nidx;
