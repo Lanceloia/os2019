90cf917e9409ed5b8cf3801be638278f23cce562
tracer-nju <tracer@nju.edu.cn>
2019-06-29 13:19:28 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 200feb0b9f714e3bac7ee02b670a6fc4   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  13:19:28 up 17:45,  1 user,  load average: 0.43, 0.98, 1.17

--- os-workbench/kernel/src/fs/procfs.c
+++ os-workbench/kernel/src/fs/procfs.c
@@ -36,7 +36,7 @@ void procfs_schdule(void *proc) {
 }
 
 int procfs_readdir(filesystem_t *fs, int ridx, int kth, vinode_t *buf) {
-  for (int k = 0, int cnt = 0; k < MAX_PROC; k++) {
+  for (int k = 0, cnt = 0; k < MAX_PROC; k++) {
     if (++cnt == kth) {
       strcpy(buf->name, procs[k].name);
       buf->ridx = procs[k].inode;
