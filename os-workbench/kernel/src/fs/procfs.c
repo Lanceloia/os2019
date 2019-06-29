613ed40b2e8beb8d08972e77f834c4016dc901b0
tracer-nju <tracer@nju.edu.cn>
2019-06-29 14:06:55 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 200feb0b9f714e3bac7ee02b670a6fc4   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  14:06:55 up 18:33,  1 user,  load average: 1.56, 1.21, 1.05

--- os-workbench/kernel/src/fs/procfs.c
+++ os-workbench/kernel/src/fs/procfs.c
@@ -8,20 +8,11 @@
  */
 
 proc_t procs[MAX_PROC];
-
-int procfs_get_idx() {
-  int idx = -1;
-  for (int i = 0; i < MAX_PROC; i++)
-    if (strlen(procs[i].name) == 0) {
-      idx = i;
-      break;
-    }
-  return idx;
-}
+int total_proc = 0;
 
 #define procfs_add(idx, _name, mode) \
   do {                               \
-    idx = procfs_get_idx();          \
+    idx = total_proc++;              \
     strcpy(procs[idx].name, _name);  \
     procs[idx].cpu_number = -1;      \
     procs[idx].schduel_times = 0;    \
@@ -34,6 +25,10 @@ int is_initialized = 0;
 
 void *procfs_addproc(const char *name) {
   if (!is_initialized) return NULL;
+  if (total_proc == MAX_PROC - 1) {
+    printf("Cannot create more proc! \n");
+    return NULL;
+  }
   int idx, mode = TYPE_FILE;
   procfs_add(idx, name, mode);
   return &procs[idx];
@@ -56,7 +51,7 @@ int procfs_init(filesystem_t *fs, const char *name, device_t *dev) {
 
 int procfs_readdir(filesystem_t *fs, int ridx, int kth, vinode_t *buf) {
   printf("here!\n");
-  for (int k = 0, cnt = 0; k < MAX_PROC; k++) {
+  for (int k = 0, cnt = 0; k < total_proc; k++) {
     if (++cnt == kth) {
       strcpy(buf->name, procs[k].name);
       buf->ridx = procs[k].inode;
