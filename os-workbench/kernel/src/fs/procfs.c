00c4253c299f31a73c86bdffcf78eed410cecd67
tracer-nju <tracer@nju.edu.cn>
2019-06-29 14:27:01 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 200feb0b9f714e3bac7ee02b670a6fc4   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  14:27:01 up 18:53,  1 user,  load average: 1.00, 1.13, 1.07

--- os-workbench/kernel/src/fs/procfs.c
+++ os-workbench/kernel/src/fs/procfs.c
@@ -65,10 +65,10 @@ int procfs_readdir(filesystem_t *fs, int ridx, int kth, vinode_t *buf) {
 
 ssize_t procfs_read(int ridx, uint64_t offset, char *buf, uint32_t len) {
   int offset = sprintf(buf, "name            cpu      schdule_times\n");
-  offset += sprintf(out + offset, "%16s", procs[ridx].name);
-  offset += sprintf(out + offset, "%3d      ", procs[ridx].cpu_number);
-  offset += sprintf(out + offset, "%d", procs[ridx].schduel_times);
-  offset += sprintf(out + offset, "\n");
+  offset += sprintf(buf + offset, "%16s", procs[ridx].name);
+  offset += sprintf(buf + offset, "%3d      ", procs[ridx].cpu_number);
+  offset += sprintf(buf + offset, "%d", procs[ridx].schduel_times);
+  offset += sprintf(buf + offset, "\n");
 }
 
 /*
