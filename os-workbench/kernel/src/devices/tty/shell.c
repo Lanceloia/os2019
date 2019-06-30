fa5c77cc481eccff0b892a23d08f51a2ece7f8c7
tracer-nju <tracer@nju.edu.cn>
2019-06-30 20:35:39 +0800

    Static hostname: lanceloia-UX310UQK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8f9faa09a8344a3d9ad3d7d221521df4            Boot ID: 589252d91b924b43bb8ea9b5870ab0f2   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-24-generic       Architecture: x86-64  20:35:39 up 45 min,  1 user,  load average: 1.39, 1.27, 0.99

--- os-workbench/kernel/src/devices/tty/shell.c
+++ os-workbench/kernel/src/devices/tty/shell.c
@@ -68,15 +68,19 @@ static void cat_do(device_t *tty, char *dirname, char *pwd) {
 static void catto_do(device_t *tty, char *dirname, char *pwd) {
   build_abs_path(dirname, pwd);
   int fd = vfs_open(abs_path, TYPE_FILE | WR_ABLE);
-  // printf("path: %s\n", abs_path);
-  while (1) {
-    int nread = tty->ops->read(tty, 0, bigbuf, sizeof(bigbuf));
-    // printf("fuck?\n");
-    if (bigbuf[nread - 2] == '~') {
-      vfs_write(fd, bigbuf, nread - 2);
-      break;
-    } else {
-      vfs_write(fd, bigbuf, nread);
+  if (fd == -1) {
+    sprintf(bigbuf, "That is not a writeable file! \n");
+    tty->ops->write(tty, 0, bigbuf, strlen(bigbuf));
+  } else {
+    while (1) {
+      int nread = tty->ops->read(tty, 0, bigbuf, sizeof(bigbuf));
+      // printf("fuck?\n");
+      if (bigbuf[nread - 2] == '~') {
+        vfs_write(fd, bigbuf, nread - 2);
+        break;
+      } else {
+        vfs_write(fd, bigbuf, nread);
+      }
     }
   }
 }
